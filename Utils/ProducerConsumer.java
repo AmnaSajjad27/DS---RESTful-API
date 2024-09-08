b7dff2c2538a93f0be0f1fd41f94d3c7e38ec196
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2024-09-08 15:40:26 +0930

    initialise function definitions

---
+++ Utils/ProducerConsumer.java
@@ -0,0 +1,66 @@
+import java.net.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
+import java.io.*;
+import org.json.*;
+
+// Initalise, load previous data and starts threads
+public void run()
+{
+
+}
+
+// Removes data older than 30 seconds
+public void clear_data()
+{
+
+}
+
+// Add request to queue 
+public void addRequest()
+{
+
+}
+
+// Get an attribute
+public String getValue()
+{
+
+}
+
+// Retrive the request
+public String getRequest()
+{
+
+}
+
+// Process the request 
+public void performRequest()
+{
+
+}
+
+// Generate HTTP response code
+private String JSON_request(int code, String message, String newLamportTime)
+{
+
+}
+
+// 
+private String get(String newLamportTime)
+{
+
+}
+
+private String put(Vector<String> data)
+{
+
+}
+
+private String lamport_time(Vector<String> request)
+{
+
+}
+
