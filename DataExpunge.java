018e7976604934825847df52830b7142017caf99
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2024-09-19 20:40:38 +1000

    GET requests not working yet

--- DataExpunge.java
+++ DataExpunge.java
@@ -1,35 +1,37 @@
 import java.util.Vector;
 
 public class DataExpunge {
-    public boolean dataExpunging() {
-        // here we will test whether data expunging after 30 seconds works
-        // a content server will send its data to the aggregation server
-        // then the client will attempt to get the content server's data
-        // after 31 seconds. if the data is null, then the data expunging works
-        ContentServer cs = new ContentServer();
-        cs.start();
+    public boolean dataExpunging()
+    {
+        ContentServer content_server = new ContentServer();
+        content_server.start();
 
-        try {
-            cs.join();
+        try 
+        {
+            content_server.join();
             Thread.sleep(31000);
 
-            GETClient c1 = new GETClient();
-            c1.start();
-            Vector<String> response = c1.getResponse();
-            c1.join();
+            GETClient client_1 = new GETClient();
+            client_1.start();
+            Vector<String> response = client_1.getResponse();
+            client_1.join();
             // if the response from the server is null
             // then the data expunging works
-            if (response == null) {
+            if (response == null)
+            {
                 return true;
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) 
+        {
             System.err.println(e.toString());
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        DataExpunge de = new DataExpunge();
-        System.out.println("\033[0;1mData Expunge works? " + de.dataExpunging() + "\033[0m");
+    public static void main(String[] args) 
+    {
+        DataExpunge data_expunge = new DataExpunge();
+        System.out.println("\033[0;1mData Expunge works? " + data_expunge.dataExpunging() + "\033[0m");
     }
 }
--- GETClient.java
+++ GETClient.java
@@ -3,31 +3,36 @@ import java.net.*;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class GETClient extends Thread {
+public class GETClient extends Thread 
+{
     private Socket socket;
-    private BufferedReader serverResponse;
-    private PrintWriter clientRequests;
+    private BufferedReader server_response;
+    private PrintWriter client_requests;
     private String clientId;
     private Integer lamportTime;
     private Vector<String> response;
 
-    public GETClient() {
+    public GETClient() 
+    {
         this.lamportTime = (int) Math.floor(Math.random() * (10 - 0 + 1) + 0);
         this.clientId = String.valueOf(Thread.currentThread().getId());
     }
 
-    public Vector<String> getResponse() {
+    public Vector<String> getResponse() 
+    {
         return this.response;
     }
 
     @Override
-    public void run() {
+    public void run() 
+    {
         BufferedReader terminalInput = new BufferedReader(new InputStreamReader(System.in));
         boolean sendRequest = true;
         System.out.println("Hello Client " + clientId + "! Please enter address and port in the format address:port");
         String address = null;
         Integer port = null;
-        try {
+        try 
+        {
             // Get the address and port from terminal input
             String temp = terminalInput.readLine();
             if (temp != null) {
@@ -35,21 +40,28 @@ public class GETClient extends Thread {
                 address = connectionDetails[0];
                 port = Integer.parseInt(connectionDetails[1]);
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) 
+        {
             System.err.println("Error with reading terminal input: " + e.toString());
         }
 
-        while (sendRequest) {
-            try {
-                if (address == null || port == null) {
-                    socket = new Socket("127.0.0.1", 4567); // Default fallback address
-                } else {
-                    socket = new Socket(address, port); // Establish socket connection
+        while (sendRequest) 
+        {
+            try 
+            {
+                if (address == null || port == null) 
+                {
+                    socket = new Socket("127.0.0.1", 4567);
+                }
+                else
+                {
+                    socket = new Socket(address, port);
                 }
 
                 // Create server response reader and client request writer
-                this.serverResponse = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                this.clientRequests = new PrintWriter(socket.getOutputStream(), true);
+                this.server_response = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.client_requests = new PrintWriter(socket.getOutputStream(), true);
 
                 System.out.println("Optional: Please enter the station id (leave empty if you want latest data):");
                 String stationId = terminalInput.readLine();
@@ -59,7 +71,8 @@ public class GETClient extends Thread {
                 String line;
 
                 // Read the response from the server line by line
-                while ((line = serverResponse.readLine()) != null) {
+                while ((line = server_response.readLine()) != null) 
+                {
                     responseLines.add(line);
                 }
 
@@ -71,18 +84,25 @@ public class GETClient extends Thread {
                 System.out.println("Request successful. Would you like to send another GET request? ('true' for yes, 'false' for no)");
                 sendRequest = Boolean.parseBoolean(terminalInput.readLine());
 
-            } catch (Exception e) {
+            } 
+            catch (Exception e) 
+            {
                 // Handle connection issues
                 System.err.println(
                         "Client " + this.clientId + " - Failed to connect to aggregation server: "
                                 + e.toString()
                                 + "\nWould you like to try connecting again? ('true' for yes, 'false' for no)");
-                try {
+                try 
+                {
                     sendRequest = Boolean.parseBoolean(terminalInput.readLine());
-                } catch (Exception err) {
+                }
+                catch (Exception err) 
+                {
                     err.printStackTrace();
                 }
-            } finally {
+            } 
+            finally 
+            {
                 // Ensure socket and streams are closed
                 closeConnections();
             }
@@ -92,44 +112,58 @@ public class GETClient extends Thread {
     }
 
     // Properly close connections
-    private void closeConnections() {
-        try {
-            if (clientRequests != null) clientRequests.close();
-            if (serverResponse != null) serverResponse.close();
+    private void closeConnections() 
+    {
+        try 
+        {
+            if (client_requests != null) client_requests.close();
+            if (server_response != null) server_response.close();
             if (socket != null) socket.close();
-        } catch (IOException e) {
+        }
+        catch (IOException e) 
+        {
             System.err.println("Error closing client resources: " + e.toString());
         }
     }
 
-    private void updateLamportTime(Vector<String> data) {
-        for (String string : data) {
-            if (string.contains("Lamport-Timestamp")) {
+    private void updateLamportTime(Vector<String> data) 
+    {
+        for (String string : data) 
+        {
+            if (string.contains("Lamport-Timestamp")) 
+            {
                 String serverTime = string.split(":")[1].strip();
                 this.lamportTime = Math.max(Integer.parseInt(serverTime), this.lamportTime) + 1;
             }
         }
     }
 
-    private void sendGetRequest(String stationId) {
-        try {
-            if (stationId == null || stationId.isEmpty()) {
-                this.clientRequests.println("GET /weather.json HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Client " +
-                        this.clientId + "\r\nLamport-Timestamp: " +
-                        this.lamportTime + "\r\nContent-Type: application/json\r\nContent-Length: 0\r\n\r\n");
-            } else {
-                this.clientRequests.println("GET /weather.json?station-id=" + stationId + " HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Client " +
+    private void sendGetRequest(String stationId) 
+    {
+        try 
+        {
+            if (stationId == null || stationId.isEmpty()) 
+            {
+                this.client_requests.println("GET /weather.json HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Client " +
                         this.clientId + "\r\nLamport-Timestamp: " +
                         this.lamportTime + "\r\nContent-Type: application/json\r\nContent-Length: 0\r\n\r\n");
             }
-            this.clientRequests.flush(); // Ensure the request is fully sent
+            else
+            {
+                this.client_requests.println("GET /weather.json?station-id=" + stationId + " HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Client " +
+                        this.clientId + "\r\nLamport-Timestamp: " +
+                        this.lamportTime + "\r\nContent-Type: application/json\r\nContent-Length: 0\r\n\r\n");
+            }
+            this.client_requests.flush(); // Ensure the request is fully sent
 
-        } catch (Exception e) {
+        } catch (Exception e) 
+        {
             System.err.println("Error sending GET request: " + e.toString());
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         GETClient client = new GETClient();
         client.start();
     }
